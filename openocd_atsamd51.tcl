f1fc5f3a83c5d54107d546da1b6e1e7ac38769b9
Kenta IDA <fuga@fugafuga.org>
2020-08-23 03:05:24 +0900

    Implement DMAC

---
+++ openocd_atsamd51.tcl
@@ -0,0 +1,32 @@
+
+set CHIPNAME samd51p19a
+source [find target/atsame5x.cfg]
+
+adapter speed 4000
+gdb_flash_program enable
+gdb_breakpoint_override hard
+
+init
+targets
+
+# for FT2232D workaround
+proc flash_bin_ft {bin_file} {
+    reset halt
+    set file_size [file size $bin_file]
+    set end_addr [expr $file_size + 0x4000]
+    for {set addr 0x4000} {$addr < $end_addr} {incr addr 0x2000} {
+        flash erase_address $addr 0x2000
+        sleep 200
+    }
+    flash write_image $bin_file 0x4000
+    verify_image $bin_file 0x4000 
+    echo "flashing $bin_file complete"
+    reset halt
+}
+proc flash_bin {bin_file} {
+    reset halt
+    flash write_image erase $bin_file 0x4000
+    verify_image $bin_file 0x4000 
+    echo "flashing $bin_file complete"
+    reset halt
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
 #![no_std]
 #![no_main]
 
+
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
                      // use panic_abort as _; // requires nightly
                      // use panic_itm as _; // logs messages over ITM; requires ITM support
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
 
-
-use wio_terminal::*;
-use wio_terminal::hal::prelude::*;
 use wio_terminal::hal::delay::Delay;
+use wio_terminal::hal::prelude::*;
+use wio_terminal::hal::{clock::GenericClockController, time::KiloHertz};
 use wio_terminal::pac::{CorePeripherals, Peripherals};
-use wio_terminal::hal::{
-    clock::GenericClockController,
-    time::KiloHertz,
-};
+use wio_terminal::*;
 
 use wio_terminal::{spi_master_lcd, spi_master_rtl8720};
 
-use ili9341::{Ili9341, Orientation};
 use embedded_graphics::{
     fonts::{Font12x16, Text},
-    pixelcolor::{Rgb565, Rgb888},
+    pixelcolor::{Rgb565},
     prelude::*,
     primitives::Circle,
     style::{PrimitiveStyle, TextStyle},
 };
+use ili9341::{Ili9341, Orientation};
+
+mod esp_at;
+use esp_at::{Dmac, AsyncOperation, OperationStatus, AsyncOperationError};
+use embedded_dma::*;
+use core::cell::RefCell;
 
 #[entry]
 fn main() -> ! {
@@ -42,22 +43,57 @@ fn main() -> ! {
         &mut peripherals.OSCCTRL,
         &mut peripherals.NVMCTRL,
     );
-    clocks.configure_gclk_divider_and_source(hal::clock::ClockGenId::GCLK0, 1, hal::clock::ClockSource::DPLL0, false);
+    clocks.configure_gclk_divider_and_source(
+        hal::clock::ClockGenId::GCLK0,
+        1,
+        hal::clock::ClockSource::DPLL0,
+        false,
+    );
 
     let mut pins = Pins::new(peripherals.PORT);
-    
+
     let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
-    
+
     let mut lcd_cs = pins.lcd_cs.into_push_pull_output(&mut pins.port);
     let mut lcd_dc = pins.lcd_dc.into_push_pull_output(&mut pins.port);
     let mut lcd_reset = pins.lcd_reset.into_push_pull_output(&mut pins.port);
     let mut lcd_backlight_ctr = pins.lcd_backlight_ctr.into_push_pull_output(&mut pins.port);
-    let mut spi_lcd = spi_master_lcd(&mut clocks, KiloHertz(60000u32), peripherals.SERCOM7, &mut peripherals.MCLK, pins.lcd_sck, pins.lcd_mosi, pins.lcd_miso, &mut pins.port);
-    
+    let mut spi_lcd = spi_master_lcd(
+        &mut clocks,
+        KiloHertz(60000u32),
+        peripherals.SERCOM7,
+        &mut peripherals.MCLK,
+        pins.lcd_sck,
+        pins.lcd_mosi,
+        pins.lcd_miso,
+        &mut pins.port,
+    );
+
     let mut rtl_cs = pins.rtl8720d_hspi_cs.into_push_pull_output(&mut pins.port);
-    let mut spi_rtl = spi_master_rtl8720(&mut clocks, KiloHertz(60000u32), peripherals.SERCOM0, &mut peripherals.MCLK, pins.rtl8720d_hspi_clk, pins.rtl8720d_hspi_mosi, pins.rtl8720d_hspi_miso, &mut pins.port);
-    
-    
+    let mut spi_rtl = spi_master_rtl8720(
+        &mut clocks,
+        KiloHertz(60000u32),
+        peripherals.SERCOM0,
+        &mut peripherals.MCLK,
+        pins.rtl8720d_hspi_clk,
+        pins.rtl8720d_hspi_mosi,
+        pins.rtl8720d_hspi_miso,
+        &mut pins.port,
+    );
+
+    let dmac = Dmac::new(peripherals.DMAC, &mut peripherals.MCLK);
+    let channel = dmac.allocate_channel(0);
+    let source = RefCell::new([0xaau8; 1024]);
+    let mut destination = RefCell::new([0u8; 1024]);
+
+    let transfer = channel.transfer_mem(source.borrow(), destination.borrow_mut(), 1024).unwrap();
+    let channel = loop {
+        match transfer.poll() {
+            OperationStatus::Running => {},
+            OperationStatus::Completed => break transfer.get_result(),
+        }
+    };
+
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     let mut lcd = Ili9341::new_spi(spi_lcd, lcd_cs, lcd_dc, lcd_reset, &mut delay).unwrap();
@@ -65,7 +101,8 @@ fn main() -> ! {
 
     lcd_backlight_ctr.set_high();
 
-    let c = Circle::new(Point::new(160, 120), 20).into_styled(PrimitiveStyle::with_fill((Rgb565::BLUE)));
+    let c = Circle::new(Point::new(160, 120), 20)
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE));
     let t = Text::new("Hello, Rust!", Point::new(0, 0))
         .into_styled(TextStyle::new(Font12x16, Rgb565::BLUE));
     c.draw(&mut lcd);
