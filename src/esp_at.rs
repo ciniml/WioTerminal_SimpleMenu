016813b0c83ff9bfb39d6b6137f202118e997b50
Kenta IDA <fuga@fugafuga.org>
2020-08-30 02:06:36 +0900

    Implement RTL8720D SPI transport

--- src/esp_at.rs
+++ src/esp_at.rs
@@ -1,62 +1,278 @@
 #![no_std]
 
-use embedded_hal::spi::{FullDuplex};
-use core::pin::Pin;
-use embedded_dma::*;
-use atsamd_hal::spi_common::CommonSpi;
-use atsamd_hal::clock::GenericClockController;
-use atsamd51p19a::{DMAC, MCLK, dmac::channel::chctrla::*};
-use core::{convert::TryFrom, convert::TryInto, cell::RefCell};
+use embedded_hal::serial;
+use embedded_hal::blocking::spi;
+use embedded_hal::digital::v2::{OutputPin, InputPin};
 
-use crate::atsamd_dmac::*;
-use crate::async_operation::*;
+use heapless::ArrayLength;
+use heapless::spsc::{Queue, Producer, Consumer};
 
 #[derive(Debug, Copy, Clone)]
-pub enum ESPTransportError {
+pub enum TransportError {
     Busy,
 }
 
-pub struct Transfer<SPI: CommonSpi, Buffer> {
-    buffer: Buffer,
-    transport: ESPTransport<SPI>,
+type ReadQueue = Queue<u8, heapless::consts::U256, u8>;
+type WriteQueue = Queue<u8, heapless::consts::U256, u8>;
+
+pub struct WioTerminalSPITransport<Spi, CsPin, IrqPin, SyncPin> 
+    where   Spi: spi::Transfer<u8> + spi::Write<u8>,
+            CsPin: OutputPin,
+            IrqPin: InputPin,
+            SyncPin: InputPin,
+{
+    spi: Spi,
+    cs_pin: CsPin,
+    irq_pin: IrqPin,
+    sync_pin: SyncPin,
+    rq: ReadQueue,
+    wq: WriteQueue,
+    state: State,
+    buffer: [u8; 1024],
 }
 
-pub struct TransferResult<SPI: CommonSpi, Buffer> {
-    transport: ESPTransport<SPI>,
-    buffer: Buffer,
-    length: usize,
+#[derive(Debug, Copy, Clone)]
+enum State {
+    Idle,
+    IssueReadData,
+    ReadDataResponseHeader {index: u8},
+    ReadDataResponseLength,
+    ReadDataResponseBody {remaining: u16, in_buffer_len: u16, in_buffer_offset: u16},
+    IssueWriteData,
 }
 
-impl <SPI: CommonSpi, Buffer> AsyncOperation<TransferResult<SPI, Buffer>> for Transfer<SPI, Buffer> {
-    fn poll(self: &Self) -> OperationStatus {
-        todo!()
-    }
-    fn get_result(self: Self) -> TransferResult<SPI, Buffer> {
-        todo!()
-    }
-    fn cancel(self: &mut Self) -> Result<(), AsyncOperationError> {
-        todo!()
-    }
-   
-}
+const SPT_TAG_PRE: u8 = 0x55u8; /* Master initiate a TRANSFER */
+const SPT_TAG_WR: u8 = 0x80u8;  /* Master WRITE  to Slave */
+const SPT_TAG_RD: u8 = 0x00u8;  /* Master READ from Slave */
+const SPT_TAG_ACK: u8 = 0xBEu8; /* Slave  Acknowledgement */
+const SPT_TAG_DMY: u8 = 0xFFu8; /* dummy */
+const SPT_ERR_OK: u8 = 0x00u8;
 
-pub struct ESPTransport<SPI: CommonSpi> {
-    spi: SPI,
+struct ScopedGuard<Func: FnMut()> {
+    func: Func,
+    suppressed: bool,
 }
-
-impl <SPI: CommonSpi> ESPTransport<SPI> {
-    fn new(spi: SPI) -> Self {
+impl <Func: FnMut()> ScopedGuard<Func> {
+    pub fn new(func: Func) -> Self {
         Self {
-            spi: spi,
+            func: func,
+            suppressed: false,
         }
     }
-    fn begin_read<'a, Buffer: WriteBuffer>(self: Self, buffer: &'a mut Buffer) -> Transfer<SPI, &'a mut Buffer> {
-        return Transfer {
-            buffer: buffer,
-            transport: self,
-        };
+    pub fn suppress(self: &mut Self) {
+        self.suppressed = true;
     }
-    fn begin_write<'a, Data: ReadBuffer>(self: Self, data: &'a Data) -> Transfer<SPI, &'a Data> {
-        todo!()
+}
+impl <Func: FnMut()> Drop for ScopedGuard<Func> {
+    fn drop(&mut self) {
+        if !self.suppressed {
+            (self.func)();
+        }
+    }
+}
+
+impl <Spi, CsPin, IrqPin, SyncPin>  WioTerminalSPITransport<Spi, CsPin, IrqPin, SyncPin> 
+    where   Spi: spi::Transfer<u8> + spi::Write<u8>,
+            CsPin: OutputPin,
+            IrqPin: InputPin,
+            SyncPin: InputPin,
+{
+    pub fn new(spi: Spi, cs_pin: CsPin, irq_pin: IrqPin, sync_pin: SyncPin) -> Self {
+        Self {
+            spi: spi,
+            cs_pin: cs_pin,
+            irq_pin: irq_pin,
+            sync_pin: sync_pin,
+            rq: Queue::u8(),
+            wq: Queue::u8(),
+            state: State::Idle,
+            buffer: [0u8; 1024],
+        }
+    }
+    
+    #[inline(always)]
+    fn assert_cs(self: &mut Self) {
+        self.cs_pin.set_low();
+    }
+    #[inline(always)]
+    fn deassert_cs(self: &mut Self) {
+        self.cs_pin.set_high();
+    }
+
+    pub fn process(self: &mut Self) {
+        loop {
+            match self.state {
+                State::Idle => {
+                    self.deassert_cs();
+                    let wq = self.wq.split().1;
+                    if wq.ready() {
+                        self.state = State::IssueWriteData;
+                    } else if let Ok(pin) = self.irq_pin.is_high() {
+                        if pin {
+                            self.state = State::IssueReadData;
+                        }
+                        else {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                },
+                State::IssueReadData => {
+                    let command = [SPT_TAG_PRE, SPT_TAG_RD];
+                    self.assert_cs();
+                    let _ = ScopedGuard::new(|| { self.deassert_cs() });
+                    if self.spi.write(&command).is_err() {
+                        // Failed to transmit.
+                        self.state = State::Idle;
+                        return;
+                    }
+                    self.state = State::ReadDataResponseHeader {index: 0};
+                },
+                State::ReadDataResponseHeader {mut index} => {
+                    let mut buffer = [0u8; 2];
+                    self.assert_cs();
+                    let _ = ScopedGuard::new(|| { self.deassert_cs() });
+                    let result = self.spi.transfer(&mut buffer[index as usize..]);
+                    if result.is_err() {
+                        self.state = State::Idle;
+                        return;
+                    }
+                    if index == 0 {
+                        // Search TAG_ACK
+                        if buffer[0] == SPT_TAG_ACK {
+                            index = 1;
+                        }
+                        else if buffer[1] == SPT_TAG_ACK {
+                            self.state = State::ReadDataResponseHeader {index: 1};
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    if index == 1 {
+                        if buffer[1] == SPT_ERR_OK {
+                            self.state = State::ReadDataResponseLength;
+                        }
+                        else {
+                            // Error response
+                            self.state = State::Idle;
+                        }
+                    }
+                },
+                State::ReadDataResponseLength => {
+                    let mut buffer = [0u8; 2];
+                    self.assert_cs();
+                    let _ = ScopedGuard::new(|| { self.deassert_cs() });
+                    let result = self.spi.transfer(&mut buffer);
+                    if result.is_err() {
+                        self.state = State::Idle;
+                        return;
+                    }
+                    let length = (buffer[0] as u16) << 8 | (buffer[1] as u16);
+                    self.state = State::ReadDataResponseBody {remaining: length, in_buffer_len: 0, in_buffer_offset: 0};
+                },
+                State::ReadDataResponseBody {mut remaining, mut in_buffer_len, mut in_buffer_offset } => {
+                    if in_buffer_len == 0 {
+                        // If there are no data in the buffer, read from the target.
+                        self.assert_cs();
+                        let _ = ScopedGuard::new(|| { self.deassert_cs() });
+                        let bytes_to_read = core::cmp::min(self.buffer.len(), remaining as usize) as u16;
+                        let result = self.spi.transfer(&mut self.buffer[..bytes_to_read as usize]);
+                        if result.is_err() {
+                            self.state = State::Idle;
+                            return;
+                        }
+                        in_buffer_len = bytes_to_read;
+                        in_buffer_offset = 0;
+                        remaining -= bytes_to_read;
+                    }
+                    
+                    // Enqueue data in the buffer
+                    let mut rq = self.rq.split().0;
+                    while in_buffer_offset < in_buffer_len {
+                        if let Err(e) = rq.enqueue(self.buffer[in_buffer_offset as usize]) {
+                            break
+                        }
+                        in_buffer_offset += 1;
+                    }
+
+                    // If there are no remaining data on the target and the local buffer, return to idle state.
+                    if remaining == 0 && in_buffer_offset == in_buffer_len {
+                        self.state = State::Idle;
+                    }
+                    else {
+                        self.state = State::ReadDataResponseBody { remaining, in_buffer_len, in_buffer_offset };
+                    }
+                },
+                State::IssueWriteData => {
+                    let mut bytes_to_write = 0usize;
+                    let mut wq = self.wq.split().1;
+                    while wq.ready() && bytes_to_write < self.buffer.len() {
+                        self.buffer[bytes_to_write] = wq.dequeue().unwrap();
+                        bytes_to_write += 1;
+                    }
+                    
+                    self.buffer[0] = SPT_TAG_PRE;
+                    self.buffer[1] = SPT_TAG_WR;
+                    self.buffer[2] = (bytes_to_write & 0xff) as u8;
+                    self.buffer[3] = (bytes_to_write >> 8) as u8;
+                    
+                    self.assert_cs();
+                    let _ = ScopedGuard::new(|| self.deassert_cs() );
+                    let result = self.spi.write(&self.buffer);
+                    if result.is_err() {
+                        self.state = State::Idle;
+                        return;
+                    }
+                    self.state = State::Idle;
+                },
+            }
+        }
+    }
+
+    pub fn split<'queue>(&'queue mut self) -> (WioTerminalSPITransportTx<'queue, heapless::consts::U256>, WioTerminalSPITransportRx<'queue, heapless::consts::U256>) {
+        (
+            WioTerminalSPITransportTx { wq: self.wq.split().0 },
+            WioTerminalSPITransportRx { rq: self.rq.split().1 },
+        )
+    }
+}
+
+pub struct WioTerminalSPITransportRx<'queue, N>  
+    where N: ArrayLength<u8>
+{
+    rq: Consumer<'queue, u8, N, u8>,
+}
+
+impl<'queue, N> serial::Read<u8> for WioTerminalSPITransportRx<'queue, N>
+    where N: ArrayLength<u8>
+{
+    type Error = TransportError;
+    fn read(&mut self) -> nb::Result<u8, Self::Error> {
+        if let Some(v) = self.rq.dequeue() {
+            Ok(v)
+        }
+        else {
+            Err(nb::Error::WouldBlock)
+        }
+    }
+}
+
+pub struct WioTerminalSPITransportTx<'queue, N> 
+    where N: ArrayLength<u8>
+{
+    wq: Producer<'queue, u8, N, u8>
+}
+
+impl<'queue, N> serial::Write<u8> for WioTerminalSPITransportTx<'queue, N>
+    where N: ArrayLength<u8>
+{
+    type Error = TransportError;
+    fn write(&mut self, word: u8) -> nb::Result<(), Self::Error> {
+        self.wq.enqueue(word).map_err(|_| nb::Error::WouldBlock)
+    }
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,18 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
+use heapless::{consts, spsc::Queue};
 
 use wio_terminal::hal::delay::Delay;
+use wio_terminal::hal::timer::TimerCounter;
 use wio_terminal::hal::prelude::*;
-use wio_terminal::hal::{clock::GenericClockController, time::KiloHertz};
+use wio_terminal::hal::{clock::GenericClockController, time::KiloHertz, time::Nanoseconds};
 use wio_terminal::pac::{CorePeripherals, Peripherals};
 use wio_terminal::*;
 
 use wio_terminal::{spi_master_lcd, spi_master_rtl8720};
+
+use embedded_hal::timer::CountDown;
 
 use embedded_graphics::{
     fonts::{Font12x16, Text},
@@ -33,9 +37,45 @@ mod async_operation;
 
 use atsamd_dmac::{Dmac};
 use async_operation::{AsyncOperation, OperationStatus, AsyncOperationError};
+use esp_at::WioTerminalSPITransport;
 
 use embedded_dma::*;
 use core::cell::RefCell;
+
+use atat::{prelude::*, atat_derive::*, ClientBuilder, ComQueue, Queues, ResQueue, UrcQueue, NoopUrcMatcher};
+
+
+
+#[derive(Clone, AtatResp)]
+pub struct NoResponse;
+
+#[derive(Clone, AtatCmd)]
+#[at_cmd("AT", NoResponse, timeout_ms = 1000)]
+pub struct AT;
+
+struct RTLTimer<Timer, TimeConverter> {
+    timer: Timer,
+    converter: TimeConverter,
+}
+impl<Timer: CountDown, TimeConverter: Fn(u32) -> Timer::Time> RTLTimer<Timer, TimeConverter> {
+    pub fn new(timer: Timer, converter: TimeConverter) -> Self {
+        Self {
+            timer: timer,
+            converter: converter,
+        }
+    }
+}
+
+impl<Timer: CountDown, TimeConverter: Fn(u32) -> Timer::Time> CountDown for RTLTimer<Timer, TimeConverter> {
+    type Time = u32;
+    fn start<T: Into<Self::Time>>(&mut self, count: T) {
+        self.timer.start((self.converter)(count.into()))
+    }
+    fn wait(&mut self) -> nb::Result<(), void::Void> {
+        self.timer.wait()
+    }
+}
+
 
 #[entry]
 fn main() -> ! {
@@ -54,16 +94,20 @@ fn main() -> ! {
         hal::clock::ClockSource::DPLL0,
         false,
     );
+    let gclk0 = clocks.gclk0();
+    let gclk5 = clocks.get_gclk(pac::gclk::pchctrl::GEN_A::GCLK5).unwrap();
+    
 
     let mut pins = Pins::new(peripherals.PORT);
+    let mut delay = Delay::new(core.SYST, &mut clocks);
 
     let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
 
-    let mut lcd_cs = pins.lcd_cs.into_push_pull_output(&mut pins.port);
-    let mut lcd_dc = pins.lcd_dc.into_push_pull_output(&mut pins.port);
-    let mut lcd_reset = pins.lcd_reset.into_push_pull_output(&mut pins.port);
+    let lcd_cs = pins.lcd_cs.into_push_pull_output(&mut pins.port);
+    let lcd_dc = pins.lcd_dc.into_push_pull_output(&mut pins.port);
+    let lcd_reset = pins.lcd_reset.into_push_pull_output(&mut pins.port);
     let mut lcd_backlight_ctr = pins.lcd_backlight_ctr.into_push_pull_output(&mut pins.port);
-    let mut spi_lcd = spi_master_lcd(
+    let spi_lcd = spi_master_lcd(
         &mut clocks,
         KiloHertz(60000u32),
         peripherals.SERCOM7,
@@ -74,8 +118,11 @@ fn main() -> ! {
         &mut pins.port,
     );
 
-    let mut rtl_cs = pins.rtl8720d_hspi_cs.into_push_pull_output(&mut pins.port);
-    let mut spi_rtl = spi_master_rtl8720(
+    let rtl_cs = pins.rtl8720d_hspi_cs.into_push_pull_output(&mut pins.port);
+    let mut rtl_chip_pu = pins.rtl8720d_chip_pu.into_push_pull_output(&mut pins.port);
+    let rtl_irq0 = pins.irq0.into_floating_input(&mut pins.port);
+    let rtl_sync = pins.sync.into_floating_input(&mut pins.port);
+    let spi_rtl = spi_master_rtl8720(
         &mut clocks,
         KiloHertz(60000u32),
         peripherals.SERCOM0,
@@ -85,21 +132,48 @@ fn main() -> ! {
         pins.rtl8720d_hspi_miso,
         &mut pins.port,
     );
+    let tc2_tc3 = clocks.tc2_tc3(&gclk5).unwrap();
+    let rtl_timer = TimerCounter::tc2_(&tc2_tc3, peripherals.TC2, &mut peripherals.MCLK);
+    let mut rtl_timer = RTLTimer::new(rtl_timer, |v| Nanoseconds(v * 1000000));
+    rtl_timer.start(1000u32);
+    let rtl_config: atat::Config = atat::Config::default();
 
-    let dmac = Dmac::new(peripherals.DMAC, &mut peripherals.MCLK);
-    let channel = dmac.allocate_channel(0);
-    let source = RefCell::new([0xaau8; 1024]);
-    let mut destination = RefCell::new([0u8; 1024]);
+    rtl_chip_pu.set_low();
+    delay.delay_ms(100u16);
+    rtl_chip_pu.set_high();
+    let mut rtl_transport = WioTerminalSPITransport::new(spi_rtl, rtl_cs, rtl_irq0, rtl_sync);
 
-    let transfer = channel.transfer_mem(source.borrow(), destination.borrow_mut(), 1024).unwrap();
-    let channel = loop {
-        match transfer.poll() {
-            OperationStatus::Running => {},
-            OperationStatus::Completed => break transfer.get_result(),
-        }
+    static mut RES_QUEUE: ResQueue<consts::U256, consts::U5> = Queue(heapless::i::Queue::u8());
+    static mut URC_QUEUE: UrcQueue<consts::U256, consts::U10> = Queue(heapless::i::Queue::u8());
+    static mut COM_QUEUE: ComQueue<consts::U3> = Queue(heapless::i::Queue::u8());
+    let queues = Queues {
+        res_queue: unsafe { RES_QUEUE.split() },
+        urc_queue: unsafe { URC_QUEUE.split() },
+        com_queue: unsafe { COM_QUEUE.split() },
+    };
+    let (mut tx, mut rx) = rtl_transport.split();
+    let (mut client, ingress) =
+        ClientBuilder::new(tx, rtl_timer, atat::Config::new(atat::Mode::Timeout))
+            .with_custom_urc_matcher(NoopUrcMatcher{})
+            .build(queues);
+    
+    let send_success = match client.send(&AT) {
+        Ok(_response) => true,
+        Err(_e) => false,
     };
 
-    let mut delay = Delay::new(core.SYST, &mut clocks);
+    // let dmac = Dmac::new(peripherals.DMAC, &mut peripherals.MCLK);
+    // let channel = dmac.allocate_channel(0);
+    // let source = RefCell::new([0xaau8; 1024]);
+    // let mut destination = RefCell::new([0u8; 1024]);
+
+    // let transfer = channel.transfer_mem(source.borrow(), destination.borrow_mut(), 1024).unwrap();
+    // let channel = loop {
+    //     match transfer.poll() {
+    //         OperationStatus::Running => {},
+    //         OperationStatus::Completed => break transfer.get_result(),
+    //     }
+    // };
 
     let mut lcd = Ili9341::new_spi(spi_lcd, lcd_cs, lcd_dc, lcd_reset, &mut delay).unwrap();
     lcd.set_orientation(Orientation::LandscapeFlipped);
@@ -111,7 +185,9 @@ fn main() -> ! {
     let t = Text::new("Hello, Rust!", Point::new(0, 0))
         .into_styled(TextStyle::new(Font12x16, Rgb565::BLUE));
     c.draw(&mut lcd);
-    t.draw(&mut lcd);
+    if send_success {
+        t.draw(&mut lcd);
+    }
     loop {
         delay.delay_ms(1000u16);
         led.toggle();
